b85aa7f32a4377db54fdcb4ae92257ad06a2cb6a
alef gabriel <alefff.gabriel@gmail.com>
2020-04-20 15:41:42 -0300

    added WTF

--- app/controls.py
+++ app/controls.py
@@ -1,6 +1,13 @@
 from flask import render_template
+from .form import MyForm
+from .model import User,db
 
 def ways(app):
-    @app.route("/", methods = ['GET'])
+    @app.route("/", methods = ['GET','POST'])
     def index():
-        return render_template('index.html')
+
+        form = MyForm()
+        # new_user = User(name=form['name'], password=form['password'],email=form['email'])
+        # db.session.add(new_user)
+        # db.session.commit()
+        return render_template('loguin.html', form=form)
--- config.py
+++ config.py
@@ -3,3 +3,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///loguin.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 DEBUG = True
+
+SECRET_KEY = 'supersecretekeygg'
